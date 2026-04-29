ed56942879ff9900abe4b5b21996f51db796eed2
mortuursed <rostikbelskij@gmail.com>
2026-04-29 16:10:40 +0200

    dfdffd

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-// const grup = "256"
-// console.log("я учусь в группе",grup)
-
-// let username;
-
-// let userage;
-
-
-
-// username = prompt("введите имя")
-
-// userage = prompt("сколько вам лет")
-
-// console.log("привет пользователь",username,"твой возраст", userage);
-
-// frontend-mentor frontend-mentor
-// 16:51
-// Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвойте змінним наступні характеристики товару (відразу при оголошенні) назва: MacBook Pro 13″, ціна: 100000
-// Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'. Присвой товару нову ціну - 20000. Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'.
-
-
-// const name = "MacBook Pro 13"
-
-// let price = 100000
-
-// console.log("обрано",name,"цена за штуку",price,"кредитов");
 
 const userage = "19";
 
@@ -35,6 +9,8 @@ console.log(name);
 
 const isStuden = true
 
+console.log(isStuden);
+
 
 const isString = "биба и боба"
 
@@ -42,7 +18,16 @@ console.log(isString);
 
 const myNull = null
 
+console.log(myNull);
 
 
+const userName = prompt("Введите имя");
 
+alert("ваше имя: " + userName);
 
+// Перевірка типу змінної
+console.log(typeof userName);
+
+const isConfirmed = confirm("хотите продолжить");
+
+alert("осторожно!");
