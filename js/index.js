c09641952932925f04734ad94cd30003149822bb
mortuursed <rostikbelskij@gmail.com>
2026-04-30 14:40:36 +0200

    fggg

--- js/index.js
+++ js/index.js
@@ -1,32 +1,134 @@
 
-const userage = "19";
+// const userage = "19";
 
-console.log(userage);
+// console.log(userage);
 
-const name = "rostislav"
+// const name = "rostislav"
 
-console.log(name);
+// console.log(name);
 
-const isStuden = true
+// const isStuden = true
 
-console.log(isStuden);
-
-
-const isString = "биба и боба"
-
-console.log(isString);
-
-const myNull = null
-
-console.log(myNull);
+// console.log(isStuden);
 
 
-const userName = prompt("Введите имя");
+// const isString = "биба и боба"
 
-alert("ваше имя: " + userName);
+// console.log(isString);
 
-console.log(typeof userName);
+// const myNull = null
 
-const isConfirmed = confirm("хотите продолжить");
+// console.log(myNull);
 
-alert("осторожно!");
+
+// const userName = prompt("Введите имя");
+
+// alert("ваше имя: " + userName);
+
+// console.log(typeof userName);
+
+// const isConfirmed = confirm("хотите продолжить");
+
+// alert("осторожно!");
+
+
+// Створити змінні celsius, fahrenheit для зберігання температури за Цельсієм та Фаренгейтом. Перевести температуру з Цельсія у Фаренгейти за допомогою формули та вивести результат в консоль.
+// Створити змінну daysInMonth для зберігання кількості днів у місяці. Обчислити кількість hoursInMonth годин та minutesInMonth хвилин у цьому місяці за допомогою оператора множення та вивести результат в консоль.
+// Створити змінні health, energy для зберігання рівня здоров'я та енергії гравця в грі. Зменшити рівень здоров'я та енергії гравця за допомогою операторів віднімання та вивести результат в консоль.
+// Створити змінну totalPrice для зберігання суми покупки в магазині. Застосувати знижку discount в розмірі 10% до цієї суми за допомогою оператора множення. Результат зберегти в змінній discountedPrice та вивести результат в консоль.
+// Створити змінну const floatNumber = 12.78 для зберігання числа з плаваючою комою. Використати метод Math.floor() для округлення числа до меншого. Результат зберегти в змінній roundedDown та вивести результат в консоль.
+// Створити змінну const floatString = "45.67"  для зберігання рядка, який містить числа з плаваючою комою. Використати метод parseFloat() для перетворення рядка у десяткове число. Результат зберегти в змінній parsedFloat та вивести результат в консоль.
+// Створити змінну const intString = "123" для зберігання рядка, який містить ціле число. Використати метод parseInt() для перетворення рядка у ціле число. Результат зберегти в змінній parsedIntта вивести результат в консоль.
+// Створити змінну number для зберігання числа. Використати метод Math.sqrt() для обчислення квадратного кореня числа. Результат зберегти в змінній sqrtNumber та вивести результат в консоль.
+// Створити змінні const integer = 42 та const stringNumber = "256" для зберігання цілочисельного значення та рядка з числом у вигляді рядка. Використати метод parseInt() для перетворення рядка у ціле число. Результат зберегти в змінній convertedInt та вивести результат в консоль. Потім використати метод toString() для перетворення цілочисельного значення у рядок. Результат зберегти в змінній convertedString та вивести результат в консоль.
+
+
+let celsius = 25
+
+let fahrenheit = (celsius*1.8)+32
+
+console.log(fahrenheit);
+
+
+
+
+let daysInMonth = 30
+
+let hours = daysInMonth*24
+
+let min =hours*60
+
+console.log(hours);
+
+console.log(min);
+
+
+
+
+let hp = 100
+
+let energy = 100
+
+console.log("health",hp-25);
+
+console.log("Stamin",energy-29);
+
+const floatNumber =12.78
+
+let roundedDown=Math.floor(floatNumber);
+
+console.log(roundedDown);
+
+
+
+let totalPrice = 500;
+
+let discount = 0.1;
+
+let discountedPrice = totalPrice*(1 - discount);
+
+console.log(discountedPrice);
+
+
+
+const floatString = "45.67";
+
+let parsedFloat = parseFloat(floatString);
+
+console.log(parsedFloat);
+
+const intString = "123";
+
+let parsedInt = parseInt(intString);
+
+console.log(parsedInt);
+
+
+
+
+let number = 64;
+
+let sqrtNumber = Math.sqrt(number);
+
+console.log(sqrtNumber);
+
+
+
+
+const integer = 42;
+
+const stringNumber = "256";
+
+let convertedInt = parseInt(stringNumber);
+
+console.log(convertedInt);
+
+let convertedString = integer.toString();
+
+console.log(convertedString);
+
+
+
+
+
+
