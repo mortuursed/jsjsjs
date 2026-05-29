db0c0b4210b516b09b4ca7735b36a769864f10f1
mortuursed <rostikbelskij@gmail.com>
2026-05-29 20:38:16 +0200

    hfyfh

---
+++ js/ja.js
@@ -0,0 +1,90 @@
+// const names = [
+//   "Александр",
+//   "Мария",
+//   "Дмитрий",
+//   "Анна",
+//   "Максим",
+//   "Елена",
+//   "Артем",
+//   "Ольга",
+//   "Иван",
+//   "Татьяна"
+// ];
+
+// const copie = names.slice()
+
+//  copie.splice(1,3,"юра")
+
+// console.log(copie);
+
+
+// const techCompanies = [
+//   "Apple",
+//   "Microsoft",
+//   "Alphabet",
+//   "Amazon",
+//   "NVIDIA",
+//   "Meta",
+//   "Tesla"
+// ];
+
+// const allNames = names.concat(techCompanies)
+
+
+// console.log(allNames);
+
+
+// const names2 = [...names,...techCompanies]
+
+// console.log(names2);
+
+// Створити масив із п'яти чисел. Використовуючи метод push(), додати до масиву ще один елемент.
+// Створити масив із трьох стрічок. Використовуючи метод pop(), видалити з масиву останній елемент.
+// Створити масив із десяти чисел. Використовуючи метод slice(), створити новий масив, який буде містити перші п'ять елементів.
+// Створити масив із п'яти чисел. Використовуючи метод indexOf(), знайти індекс елемента зі значенням 3.
+// Створити масив із п'яти стрічок. Використовуючи метод join(), створити одну стрічку, яка буде містити всі елементи масиву, розділені комою.
+
+
+// const numbers =[
+//     2,54,43,23,11
+// ]
+
+// numbers.push(34)
+
+// console.log(numbers);
+
+
+// const str = ["валя","серега","серж"]
+
+// str.pop(2,1)
+
+// console.log(str);
+
+
+// const num = [1,2,3,4,5,6,7,8,9,10]
+
+// const copie = num.slice()
+
+// copie.splice(5,5)
+
+// console.log(copie);
+
+
+// const num = [1,43,23,55,334,3]
+
+// const index = num.indexOf(3)
+
+// num.splice(index,1)
+
+// console.log(num);
+
+
+// const text =["lol","kek","bob","lex","deus"]
+
+// const res = text.join(" ")
+
+// console.log(res);
+
+
+
+
--- js/taskfive.js
+++ js/taskfive.js
@@ -1,9 +1,9 @@
-// for (let i = 1; i <= 10; i += 1) {
+// for (let i = 0; i <= 10; i += 1) {
 //     console.log(i);
 // }
 
 
-// for (let i = 2; i <= 20; i += 1) 
+// for (let i = 0; i <= 20; i += 1) 
 //     if (i % 2 !== 0) {
 //         continue
 //     }
@@ -14,23 +14,58 @@
 
 //     const n = 3.14; 
 
-// for (let i = 1; i <= 3.14; i += 1) {
+// for (let i = 0; i <= 3.14; i += 1) {
 //     if (i >= n) {
 //         break;
 //     }
+// }
 
 
-    let i = 1;
 
-while (i <= 20) {
-    if (i % 3 === 0) {
-        i += 1
-        continue;
-    }
+//     let i = 0;
 
-    console.log(i)
-    i +=1
-}
+// while (i <= 20) {
+//     if (i % 3 === 0) {
+//         i += 1
+//         continue;
+//     }
+
+//     console.log(i)
+//     i +=1
+// }
+
+
+
+// ==========================================================
+
+//  const text =["mango","alex","poly","kiwi"]
+
+// const res = text.join(",")
+
+// console.log(res);
+
+
+
+
+const card = [
+    'card-1',
+    'card-2',
+    'card-3',
+    'card-4',
+    'card-5',
+]
+ 
+const remove = card.indexOf('card-3')
+
+card.splice(remove,1)
+
+card.push("card-6")
+
+card.splice(2,1,'card-8')
+
+console.log(card);
+
+
 
 
     
