2f521bef2e9cbb62783efbd10cf8b6606a7e60a7
mortuursed <rostikbelskij@gmail.com>
2026-07-27 14:14:50 +0200

    а

--- js/vyszl.js
+++ js/vyszl.js
@@ -4,107 +4,109 @@
 
 // console.log(textEl.innerHTML);
 // ====================================
-const listEl = document.querySelector(".list")
+// const listEl = document.querySelector(".list")
 
-const massName = ["oleg","vasa","peter","anna"]
+// const massName = ["oleg","vasa","peter","anna"]
 
-function creatName (array) {
-    const item = array.map((name) => {
-        return `<li>${name}</li>`             
-    }).join("")
-    // listEl.innerHTML = item
-    listEl.insertAdjacentHTML("beforeend",item)
-}
+// function creatName (array) {
+//     const item = array.map((name) => {
+//         return `<li>${name}</li>`             
+//     }).join("")
+//     // listEl.innerHTML = item
+//     listEl.insertAdjacentHTML("beforeend",item)
+// }
 
-creatName(massName)
+// creatName(massName)
 
 
-const appleMass = ["apple","orange","banana"]
+// const appleMass = ["apple","orange","banana"]
 
-creatName(appleMass)
+// creatName(appleMass)
 
-const users = [
-  {
-    id: 1,
-    name: "Іван",
-    age: 20,
-    city: "Київ",
-  },
-  {
-    id: 2,
-    name: "Олена",
-    age: 25,
-    city: "Львів",
-  },
-  {
-    id: 3,
-    name: "Максим",
-    age: 22,
-    city: "Одеса",
-  },
-  {
-    id: 4,
-    name: "Софія",
-    age: 19,
-    city: "Харків",
-  },
-  {
-    id: 5,
-    name: "Андрій",
-    age: 30,
-    city: "Дніпро",
-  },
-  {
-    id: 6,
-    name: "Марія",
-    age: 27,
-    city: "Чернівці",
-  },
-  {
-    id: 7,
-    name: "Дмитро",
-    age: 24,
-    city: "Івано-Франківськ",
-  },
-  {
-    id: 8,
-    name: "Наталія",
-    age: 28,
-    city: "Тернопіль",
-  },
-  {
-    id: 9,
-    name: "Владислав",
-    age: 21,
-    city: "Ужгород",
-  },
-  {
-    id: 10,
-    name: "Катерина",
-    age: 26,
-    city: "Полтава",
-  },
-];
+// const users = [
+//   {
+//     id: 1,
+//     name: "Іван",
+//     age: 20,
+//     city: "Київ",
+//   },
+//   {
+//     id: 2,
+//     name: "Олена",
+//     age: 25,
+//     city: "Львів",
+//   },
+//   {
+//     id: 3,
+//     name: "Максим",
+//     age: 22,
+//     city: "Одеса",
+//   },
+//   {
+//     id: 4,
+//     name: "Софія",
+//     age: 19,
+//     city: "Харків",
+//   },
+//   {
+//     id: 5,
+//     name: "Андрій",
+//     age: 30,
+//     city: "Дніпро",
+//   },
+//   {
+//     id: 6,
+//     name: "Марія",
+//     age: 27,
+//     city: "Чернівці",
+//   },
+//   {
+//     id: 7,
+//     name: "Дмитро",
+//     age: 24,
+//     city: "Івано-Франківськ",
+//   },
+//   {
+//     id: 8,
+//     name: "Наталія",
+//     age: 28,
+//     city: "Тернопіль",
+//   },
+//   {
+//     id: 9,
+//     name: "Владислав",
+//     age: 21,
+//     city: "Ужгород",
+//   },
+//   {
+//     id: 10,
+//     name: "Катерина",
+//     age: 26,
+//     city: "Полтава",
+//   },
+// ];
 
-function creatUsers (array) {
-    const item = array.map(({id,name,age,city}) => {
-        return `<li id = "${id}">
-    <h2>${name}</h2>
-    <p>${age}</p>
-    <p>${city}</p>
-    </li>`
-    }).join("")
-    listEl.insertAdjacentHTML("beforeend",item)
-}
+// function creatUsers (array) {
+//     const item = array.map(({id,name,age,city}) => {
+//         return `<li id = "${id}">
+//     <h2>${name}</h2>
+//     <p>${age}</p>
+//     <p>${city}</p>
+//     </li>`
+//     }).join("")
+//     listEl.insertAdjacentHTML("beforeend",item)
+// }
 
-creatUsers(users)
+// creatUsers(users)
 
-const buttonEl = document.querySelectorAll(".button")
+// const buttonEl = document.querySelectorAll(".button")
 
-buttonEl.forEach((btn) => {
-    console.log(btn.dataset.action);
+// buttonEl.forEach((btn) => {
+//     console.log(btn.dataset.action);
     
-})
+// })
+
+
 
 
 
